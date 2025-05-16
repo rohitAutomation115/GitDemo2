114cfd3811c861fefc443ce8dcd8d9a6ad8ce49f
rohitAutomation115 <r11joshi5@gmail.com>
2025-05-16 11:32:01 +0530

    commit from US guy

--- src/tcs_digitalproject/gitdemo.java
+++ src/tcs_digitalproject/gitdemo.java
@@ -5,6 +5,9 @@ public class gitdemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git demo to understand");
+		System.out.println("Git demo to understand2");
+		System.out.println("Git demo to understand3");
+		System.out.println("Git demo to understand4");
 	}
 
 }
